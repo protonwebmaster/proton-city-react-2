65629db9f5a1d937d4612d0fbfa383a392deed1b
scarabator <exonet3i@gmail.com>
2020-07-02 14:33:54 +0200

    dublincore commit

---
+++ build/precache-manifest.77b4338a77f484aa670d7af91254f9dc.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494f3a1571f36cbded0a648ebc7672c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ea7cd364255b7ff76c1",
+    "url": "/static/css/2.4e4d700a.chunk.css"
+  },
+  {
+    "revision": "d9ec85f7d1ea09748ed8",
+    "url": "/static/css/main.d3c1dd01.chunk.css"
+  },
+  {
+    "revision": "2ea7cd364255b7ff76c1",
+    "url": "/static/js/2.6ce1b77d.chunk.js"
+  },
+  {
+    "revision": "c1475f0726a4375733ad47043bee3c55",
+    "url": "/static/js/2.6ce1b77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ec85f7d1ea09748ed8",
+    "url": "/static/js/main.ec2a8078.chunk.js"
+  },
+  {
+    "revision": "63544584c900ce7838f8",
+    "url": "/static/js/runtime-main.ad331ff5.js"
+  },
+  {
+    "revision": "465a510535ff132c24d5c15673367756",
+    "url": "/static/media/Elate-min.465a5105.jpg"
+  },
+  {
+    "revision": "5a5245f9ce705db2314ff93c593a2390",
+    "url": "/static/media/Protonwebmaster-tech2.5a5245f9.pdf"
+  },
+  {
+    "revision": "14be1c6f14786c3bd07a5989392f1bda",
+    "url": "/static/media/analytics-min.14be1c6f.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "a88e4fb95d2a32b5ba7ef4a90f19f58b",
+    "url": "/static/media/bg-min.a88e4fb9.jpg"
+  },
+  {
+    "revision": "8e957538f98552231d8ee355b36b4c6d",
+    "url": "/static/media/bg2-min.8e957538.jpg"
+  },
+  {
+    "revision": "27a7e748d43ac57fc063826b7c0912aa",
+    "url": "/static/media/bg3-min.27a7e748.jpg"
+  },
+  {
+    "revision": "0f78dc821e7b4e7a733930491f8348e2",
+    "url": "/static/media/bg4-min.0f78dc82.jpg"
+  },
+  {
+    "revision": "04df0fcefc6823a4e22aaeedc4392cbc",
+    "url": "/static/media/bg7-min.04df0fce.jpg"
+  },
+  {
+    "revision": "a8de10d7cb4c43161e8c3e7d54d334b4",
+    "url": "/static/media/carte-min.a8de10d7.jpg"
+  },
+  {
+    "revision": "f3db89cc391f5d1a283606e9740eef12",
+    "url": "/static/media/cartem-min.f3db89cc.jpg"
+  },
+  {
+    "revision": "df0b0558fa71b6d932dbdfd4d43e7d0b",
+    "url": "/static/media/cdc-site-pdf.df0b0558.pdf"
+  },
+  {
+    "revision": "508eac41c454ebf1b7b2f68043d102e2",
+    "url": "/static/media/charteGraphique.508eac41.pdf"
+  },
+  {
+    "revision": "6a6e450730a0c4fd8ebf3a096c3d01fb",
+    "url": "/static/media/chezmarie-min.6a6e4507.jpg"
+  },
+  {
+    "revision": "a49d88854b65c0b8f52797ec3d090d1f",
+    "url": "/static/media/chezmarie-site-min.a49d8885.jpg"
+  },
+  {
+    "revision": "718de9ea2c15cbed5419f7dcc07d0961",
+    "url": "/static/media/contrat-creation-site.718de9ea.pdf"
+  },
+  {
+    "revision": "620dc6e08de9844acc11dbaa9ff6b90f",
+    "url": "/static/media/database-min.620dc6e0.png"
+  },
+  {
+    "revision": "b5fad59f3d286bc1e386dfa0a439bd0e",
+    "url": "/static/media/dns-min.b5fad59f.png"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "9a68b0613e7493d15b9e3e93dff95cae",
+    "url": "/static/media/facebook-analytics-min.9a68b061.png"
+  },
+  {
+    "revision": "1a172fbaf72a276f746fec6c6886eddf",
+    "url": "/static/media/flyer-min.1a172fba.jpg"
+  },
+  {
+    "revision": "b3e24adeeaa914d75f4fd095a6fa0442",
+    "url": "/static/media/flyersm-min.b3e24ade.jpg"
+  },
+  {
+    "revision": "3ede1261f76fe4c1137fb499541d014b",
+    "url": "/static/media/landing-bg-min.3ede1261.jpg"
+  },
+  {
+    "revision": "dbbffacad590312fc055917ba9b0efd1",
+    "url": "/static/media/landing-min.dbbffaca.jpg"
+  },
+  {
+    "revision": "ff3c2f78a58d996947cda08a4fca5b22",
+    "url": "/static/media/logo-protonwebmaster.ff3c2f78.png"
+  },
+  {
+    "revision": "9d4c5f2f5ae454bfdb413726daa056c8",
+    "url": "/static/media/logo.9d4c5f2f.png"
+  },
+  {
+    "revision": "992d018f02303ac4cca6103074407586",
+    "url": "/static/media/logoex-min.992d018f.jpg"
+  },
+  {
+    "revision": "42890bcec37c7eec0ada3cd86a01feef",
+    "url": "/static/media/mobile-app-min.42890bce.png"
+  },
+  {
+    "revision": "b0cc9c1e94565c4b92586cb4d8981e43",
+    "url": "/static/media/mockup-min.b0cc9c1e.jpg"
+  },
+  {
+    "revision": "c51d244efb190adf218bc6bfc726ef65",
+    "url": "/static/media/mockup250-min.c51d244e.jpg"
+  },
+  {
+    "revision": "2911fca4d86482866f2060c5c8f98265",
+    "url": "/static/media/netlify-min.2911fca4.png"
+  },
+  {
+    "revision": "c840561993c859f8d50464c0fea25114",
+    "url": "/static/media/netlify-min.c8405619.jpg"
+  },
+  {
+    "revision": "8a06a4f69c044f115499cd4ba8a0a0ad",
+    "url": "/static/media/okta-min.8a06a4f6.png"
+  },
+  {
+    "revision": "1c7ca251f5991ce591bf9321cb96377f",
+    "url": "/static/media/otketo-min.1c7ca251.jpg"
+  },
+  {
+    "revision": "b1962978ef61b5ba9342823a7d406ac0",
+    "url": "/static/media/prestashop-min.b1962978.png"
+  },
+  {
+    "revision": "e09282819817cdf7e62b0cc220c0562d",
+    "url": "/static/media/prestashopedit-min.e0928281.jpg"
+  },
+  {
+    "revision": "476c6ff4965802ad6290db99ccc06c4e",
+    "url": "/static/media/product-min.476c6ff4.jpg"
+  },
+  {
+    "revision": "363b70cdf3908fc4a3b9e26edab9c079",
+    "url": "/static/media/profile-min.363b70cd.jpg"
+  },
+  {
+    "revision": "55a82fff4935c3850c4ed4d1c80d5938",
+    "url": "/static/media/rancher-min.55a82fff.png"
+  },
+  {
+    "revision": "7a89ce2a78f273b923886bbbdd9a71ae",
+    "url": "/static/media/security-min.7a89ce2a.png"
+  },
+  {
+    "revision": "4061b052cc4d1754f9ed3208afbed555",
+    "url": "/static/media/segment-min.4061b052.png"
+  },
+  {
+    "revision": "188948db68c83cf0caae0d23953c3dc0",
+    "url": "/static/media/seo-min.188948db.png"
+  },
+  {
+    "revision": "7defec1fd320fee0fafd843d946f25a6",
+    "url": "/static/media/site-min.7defec1f.png"
+  },
+  {
+    "revision": "536c956da775847bb9ea6437a96a2a9d",
+    "url": "/static/media/skype-min.536c956d.png"
+  },
+  {
+    "revision": "72b39db6eba4de3feca81d0b2cb4ac9b",
+    "url": "/static/media/stripelog-min.72b39db6.jpg"
+  },
+  {
+    "revision": "2f528801dfb61ed787cce1384e26adaa",
+    "url": "/static/media/themewpedit-min.2f528801.jpg"
+  },
+  {
+    "revision": "71a8a304e86cbccc8588feb5207fe53b",
+    "url": "/static/media/visa-min.71a8a304.jpg"
+  },
+  {
+    "revision": "e51de46278a5bd5300e0dd31b1190714",
+    "url": "/static/media/webhook-1-min.e51de462.jpg"
+  },
+  {
+    "revision": "285a925cf180a5e12d0e238ad1c2ba07",
+    "url": "/static/media/webhook-2-min.285a925c.jpg"
+  },
+  {
+    "revision": "e7f5d5c0658936ab70b1a60bf3b2535d",
+    "url": "/static/media/wireframe400.e7f5d5c0.jpg"
+  },
+  {
+    "revision": "e2afb197474811ca5ea0f61698493913",
+    "url": "/static/media/wireframe500.e2afb197.jpg"
+  },
+  {
+    "revision": "b0f06bdd3926474a084d07fb576544d6",
+    "url": "/static/media/woo-min.b0f06bdd.png"
+  },
+  {
+    "revision": "acbe8c3da281e2c234507f564b713df5",
+    "url": "/static/media/wordpress-min.acbe8c3d.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f7eec060a6b4847a29ab6b06ea3d450.js"
+  "/precache-manifest.77b4338a77f484aa670d7af91254f9dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
